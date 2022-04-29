f90f6706d5117872a6a259942864ef8f088056a5
valeriorizzi <103269682+valeriorizzi@users.noreply.github.com>
2022-04-29 16:22:57 +0200

    Add files via upload

--- 2Dpotential/opes/run_all.sh
+++ 2Dpotential/opes/run_all.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+export LC_NUMERIC="en_US.UTF-8"
 
 #first manually create empty directories named with progressive numbers
 #this will launch an indipendent replica of the system in each of the directories
+barrier=5
+num_sim=50
+rm times.dat
+
+#creat folders
+for (( i=1; i<=$num_sim; i++ )); do
+  mkdir "$i"
+done
 
 directories="`ls -d [0-9]` `ls -d [0-9][0-9]`"
 totrep=`echo $directories |wc -w`
@@ -12,5 +21,34 @@ do
   echo "--- rep $i:"
   cd $i
   ../../queue_md.i.sh $i &
+  pids[${i}]=$!
   cd ..
 done
+
+# wait for all pids before calculating the time
+for pid in ${pids[*]}; do
+  wait $pid
+done
+
+for i in $directories
+do
+  cd $i
+  echo "time calculation --- rep $i:"
+  tail -n +2 COLVAR | awk '{a=a+exp(($5+bar)/1.0);b=exp(($5+bar)/1.0);c=c+0.5*b;{print $1,$2,c/10.0^0,($1/10.0^0)*(a/(NR+1))}}' bar=$barrier > time 
+  tempo=`tail -n 1 time | awk '{print $4}'`
+  printf "%e\n" $tempo >> ../times.dat
+  temposim=`tail -n 1 time | awk '{print $1}'`
+  printf "%e\n" $temposim >> ../times_sim.dat
+  cd ..
+done
+
+rm running_mean-std_times.dat mean-std_times.dat running_mean-std_times_sim.dat mean-std_times_sim.dat
+awk '{for(i=1;i<=NR;i++) {sum += $i; sumsq += ($i)^2}};{printf "%e %e \n", sum/NR, sqrt((sumsq-sum^2/NR)/NR)}' times.dat >> running_mean-std_times.dat
+awk '{for(i=1;i<=NR;i++) {sum += $i; sumsq += ($i)^2}};{printf "%e %e \n", sum/NR, sqrt((sumsq-sum^2/NR)/NR)}' times_sim.dat >> running_mean-std_times_sim.dat
+tail -n 1 running_mean-std_times.dat > mean-std_times.dat
+tail -n 1 running_mean-std_times_sim.dat >> mean-std_times.dat
+rm running*
+
+cat mean-std_times.dat
+#export PATH="/home/vrizzi@iit.local/programs/miniconda3/bin:$PATH"
+python3 KStest.py
